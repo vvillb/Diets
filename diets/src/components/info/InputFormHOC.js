7bcbee391a0003a401c959d114a5ccc46202de2e
Virginia Villalobos <virginiavt98@gmail.com>
2023-03-09 09:30:06 +0100

    HOC form works well

--- diets/src/components/info/InputFormHOC.js
+++ diets/src/components/info/InputFormHOC.js
@@ -25,56 +25,59 @@ export default class InputFormHOC extends React.Component {
     customer.age=event.target.value;
 
     this.setState({customer:customer});
-  };
+  }
 
    handleGenderChanged = (event) => {
     var customer= this.state.customer;
     customer.gender=event.target.value;
 
     this.setState({customer:customer});
-  };
+  }
   handleWeightChanged = (event) => {
     var customer= this.state.customer;
     customer.weight=event.target.value;
 
     this.setState({customer:customer});
-  };
+  }
   handleHeightChanged = (event) => {
     var customer= this.state.customer;
     customer.height=event.target.value;
 
     this.setState({customer:customer});
-  };
+  }
   handleActivityLevelChanged = (event) => {
     var customer= this.state.customer;
     customer.activityLevel=event.target.value;
 
     this.setState({customer:customer});
-  };
-   handleFoodChanged = (event) => {
-    var customer= this.state.customer;
-    customer.food=event.target.value;
-
-    this.setState({customer:customer});
-  };
+  }
+  handleFoodChanged = (event) => {
+    const selectedFood = event.target.value;
+    this.setState(prevState => ({
+      customer: {
+        ...prevState.customer,
+        selectedFood
+      }
+    }));
+  }
   
 
    handleSubmit = (event) => {
-   console.log({age, gender, weight, height, activityLevel})
+   console.log(this.state.customer);
     event.preventDefault();
     // Handle form submission
-  };
+  }
   render(){
   return (
-    <form onSubmit={handleSubmit} class="row gy-2 gx-3 align-items-center">
+    <form onSubmit={this.handleSubmit.bind(this)} class="row gy-2 gx-3 align-items-center">
       <div class="col-auto">
         <label htmlFor="age">Age:</label>
         <input
           type="number"
           className="form-control"
           id="age"
-          value={age}
-          onChange={handleAgeChange}
+          value={this.state.customer.age}
+          onChange={this.handleAgeChanged.bind(this)}
         />
       </div>
       <div class="col-auto">
@@ -82,8 +85,8 @@ export default class InputFormHOC extends React.Component {
         <select
           className="form-control"
           id="gender"
-          value={gender}
-          onChange={handleGenderChange}
+          value={this.state.customer.gender}
+          onChange={this.handleGenderChanged.bind(this)}
         >
           <option value="">Select Gender</option>
           <option value="male">Male</option>
@@ -97,8 +100,8 @@ export default class InputFormHOC extends React.Component {
           type="number"
           className="form-control"
           id="weight"
-          value={weight}
-          onChange={handleWeightChange}
+          value={this.state.customer.weight}
+          onChange={this.handleWeightChanged.bind(this)}
         />
       </div>
       <div class="col-auto">
@@ -107,8 +110,8 @@ export default class InputFormHOC extends React.Component {
           type="number"
           className="form-control"
           id="height"
-          value={height}
-          onChange={handleHeightChange}
+          value={this.state.customer.height}
+          onChange={this.handleHeightChanged.bind(this)}
         />
       </div>
       <div class="col-auto">
@@ -119,8 +122,8 @@ export default class InputFormHOC extends React.Component {
           id="activityLevel"
           min="0"
           max="10"
-          value={activityLevel}
-          onChange={handleActivityLevelChange}
+          value={this.state.customer.activityLevel}
+          onChange={this.handleActivityLevelChanged.bind(this)}
         />
       </div>
       <div className="form-group">
@@ -132,8 +135,8 @@ export default class InputFormHOC extends React.Component {
               type="radio"
               name="food"
               value={food.name}
-              checked={selectedFood === food.name}
-              onChange={handleFoodChange}
+              checked={this.state.customer.selectedFood === food.name}
+              onChange={this.handleFoodChanged}
             />
             <label className="form-check-label">{food.name}</label>
           </div>
