c23021bc205548f3afe8f33ed1574ca18218a89e
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-01 17:57:24 +0200

    pyodide component logs message on the console

--- diets/src/pages/Home.js
+++ diets/src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import MealMaker from '../components/meal_maker/MealMaker'
-
+import Pyodide from '../components/meal_maker/Pyodide'
 
 function Home() {
     
   return (
     <div>
+      <Pyodide pythonCode={
+        `
+        import random
+        def shuffle(data):
+            for i in range(len(data) - 1):
+              j = random.randrange(i, len(data))
+              data[i], data[j] = data[j], data[i]
+        data = [0, 1, 2, 3, 4, 5]
+        shuffle(data)
+        print(data)`
+      }
+/>
+
       <h1>Generar dietas</h1>
         <MealMaker/>
     </div>
@@ -14,3 +27,5 @@ function Home() {
 
 
 export default Home
+
+
