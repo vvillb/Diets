6d54412b96fce53edfffec4c9bca3c67d8a6d52f
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-04 12:21:53 +0200

    fixed from (i think)

--- diets/src/components/meal_maker/MealMaker.js
+++ diets/src/components/meal_maker/MealMaker.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import CalculateMacros from '../info/CalculateMacros';
 import FormCalc from '../info/FormCalc';
 import PyodidePoke from './PyodidePoke';
@@ -21,16 +21,48 @@ function MealMaker() {
     const[scoops,setScoops]=useState(1);
     const[fruit,setFruit]=useState(2);
     const [formSubmitted, setFormSubmitted] = useState(false);
+    const [dailyCarbTarget, setDailyCarbTarget] = useState(0);
+    const [dailyProteinTarget, setDailyProteinTarget] = useState(0);
+    const [dailyFatTarget, setDailyFatTarget] = useState(0);
+    const [targetBreakfastCarbs, setTargetBreakfastCarbs] = useState(0);
+    const [targetBreakfastProtein, setTargetBreakfastProtein] = useState(0);
+    const [targetBreakfastFats, setTargetBreakfastFats] = useState(0);
+    const [targetLunchDinnerCarbs, setTargetLunchDinnerCarbs] = useState(0);
+    const [targetLunchDinnerProtein, setTargetLunchDinnerProtein] = useState(0);
+    const [targetLunchDinnerFats, setTargetLunchDinnerFats] = useState(0);
 
-    function handleMacrosChange(protein, carbs, fats,scoops,fruit) {
+
+    useEffect(() => {
+      const remainingCarbs = dailyCarbTarget - 30 * fruit;
+      const remainingProtein = dailyProteinTarget - 23 * scoops;
+      const remainingFat = dailyFatTarget;
+      setTargetBreakfastCarbs(Math.round(remainingCarbs * 0.22));
+      setTargetBreakfastProtein(Math.round(remainingProtein * 0.22));
+      setTargetBreakfastFats(Math.round(remainingFat * 0.22));
+      const remainingCarbsForLunchDinner = Math.round(
+        (remainingCarbs - targetBreakfastCarbs) / 2
+      );
+      const remainingProteinForLunchDiner= Math.round(
+        (remainingCarbs - targetBreakfastProtein) / 2
+      );
+      setTargetLunchDinnerCarbs(remainingCarbsForLunchDinner);
+      setTargetLunchDinnerProtein(remainingProteinForLunchDiner);
+      setTargetLunchDinnerFats(Math.round(remainingFat / 2));
+    }, [protein, carbs, fats, scoops, fruit, dailyCarbTarget, dailyProteinTarget, dailyFatTarget,targetBreakfastCarbs,targetBreakfastProtein]);
+  
+    function handleMacrosChange(protein, carbs, fats, scoops, fruit) {
       setProtein(protein);
       setCarbs(carbs);
       setFats(fats);
       setScoops(scoops);
       setFruit(fruit);
       setFormSubmitted(true);
+      setDailyCarbTarget(carbs-(30*fruit));
+      setDailyProteinTarget(protein -(23*scoops));
+      setDailyFatTarget(fats);
     }
-    const distribution= CalculateMacros(carbs, fats, protein,scoops,fruit);
+  
+
   return (
     <div>
       <div>
@@ -40,15 +72,15 @@ function MealMaker() {
       {formSubmitted && (
       <div>
         <div>
-        <p>Los macros diarios para las comidas son: {distribution.dailyCarbTarget} gramos de hidratos, {distribution.dailyProteinTarget} gramos 
-          de proteínas y {distribution.dailyFatTarget} gramos de grasas.
+        <p>Los macros diarios para las comidas son: {dailyCarbTarget} gramos de hidratos, {dailyProteinTarget} gramos 
+          de proteínas y {dailyFatTarget} gramos de grasas.
           La distribución de los macros en las comidas será: 
-          Desayuno: {distribution.targetBreakfastCarbs} gramos de hidratos, 
-          {distribution.targetBreakfastProtein} gramos de proteína y {distribution.targetBreakfastFats}
-          gramos de grasas. Comida: {distribution.targetLunchDinnerCarbs} gramos de hidratos, 
-          {distribution.targetLunchDinnerProtein} gramos de proteína y {distribution.targetLunchDinnerFats} gramos de grasas.
-          E igualmente para la cena: {distribution.targetLunchDinnerCarbs} gramos de hidratos, 
-          {distribution.targetLunchDinnerProtein} gramos de proteína y {distribution.targetLunchDinnerFats} gramos de grasas.
+          Desayuno: {targetBreakfastCarbs} gramos de hidratos, 
+          {targetBreakfastProtein} gramos de proteína y {targetBreakfastFats}
+          gramos de grasas. Comida: {targetLunchDinnerCarbs} gramos de hidratos, 
+          {targetLunchDinnerProtein} gramos de proteína y {targetLunchDinnerFats} gramos de grasas.
+          E igualmente para la cena: {targetLunchDinnerCarbs} gramos de hidratos, 
+          {targetLunchDinnerProtein} gramos de proteína y {targetLunchDinnerFats} gramos de grasas.
           Tomando en cuenta que se han cogido {fruit} porciones de fruta,  las cuales
           restan { 30*fruit} grs de carbohidratos, y {scoops} de proteína, que restan unos  
           { 23*scoops} gramos de proteína.
@@ -60,30 +92,30 @@ function MealMaker() {
         <h5>Tostadas con jamón cocido:</h5>
         
 
-        <TostasJamonCocido var1={distribution.targetBreakfastCarbs} var2={distribution.targetBreakfastProtein} var3={distribution.targetBreakfastFats}/>
+        <TostasJamonCocido var1={targetBreakfastCarbs} var2={targetBreakfastProtein} var3={targetBreakfastFats}/>
         <h5>Tostadas con Jamón Serrano y tomate + copos de avena:</h5>
-        <TostasJamonSerrano var1={distribution.targetBreakfastCarbs} var2={distribution.targetBreakfastProtein} var3={distribution.targetBreakfastFats}/>
+        <TostasJamonSerrano var1={targetBreakfastCarbs} var2={targetBreakfastProtein} var3={targetBreakfastFats}/>
         <h5>Smoothie Bowl:</h5>
-        <SmoothieBowl var1={distribution.targetBreakfastCarbs} var2={distribution.targetBreakfastProtein} var3={distribution.targetBreakfastFats}/>
+        <SmoothieBowl var1={targetBreakfastCarbs} var2={targetBreakfastProtein} var3={targetBreakfastFats}/>
         <h5>Huevos Revueltos:</h5>
-        <HuevosRevueltos  var1={distribution.targetBreakfastCarbs} var2={distribution.targetBreakfastProtein} var3={distribution.targetBreakfastFats}/>
+        <HuevosRevueltos  var1={targetBreakfastCarbs} var2={targetBreakfastProtein} var3={targetBreakfastFats}/>
       </div>
       <div>
         <h4>Opciones para la comida y cena :</h4>
         <h5>Poke Bowl:</h5>
-        <PyodidePoke var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <PyodidePoke var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Pasta con pollo y queso</h5>
-        <PastaPolloQueso var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <PastaPolloQueso var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Pescado con arroz y aceitunas + chocolate 85% y yogur</h5>
-        <PescadoArroz var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <PescadoArroz var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Gnocchi con pollo</h5>
-        <GnocchiPollo var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <GnocchiPollo var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Ternera con patatas y aceitunas</h5>
-        <TerneraPatataAceitunas var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <TerneraPatataAceitunas var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Ensalada de garbanzos</h5>
-        <EnsaladaGarbanzos var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <EnsaladaGarbanzos var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
         <h5>Ternera con patatas y un yogur </h5>
-        <TerneraPatataYogur var1={distribution.targetLunchDinnerCarbs} var2={distribution.targetLunchDinnerProtein} var3={distribution.targetLunchDinnerFats}/>
+        <TerneraPatataYogur var1={targetLunchDinnerCarbs} var2={targetLunchDinnerProtein} var3={targetLunchDinnerFats}/>
       </div>
        
       </div>)}
